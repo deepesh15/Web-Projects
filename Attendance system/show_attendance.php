a18de932bc30dd8afb37b0e05ccd455100286aad
Deepesh Yadav <39853188+deepesh15@users.noreply.github.com>
2019-02-13 00:18:00 +0530

    Add files via upload

--- Attendance system/show_attendance.php
+++ Attendance system/show_attendance.php
@@ -1,11 +1,13 @@
 <?php
+    /* Once the include path is set it is easy to include the file by name alone */
+    set_include_path( __DIR__ . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR );
+?>
+
+<?php
 include("db.php");
 include("header.php");
 
-
-?>
-
-
+?> 
 
 
 <!DOCTYPE html>
@@ -30,39 +32,47 @@ include("header.php");
 <div class ="panel panel-default">
     <div class="panel panel-heading">
     <h2>
-    
-
     <a class ="btn btn-success" href="add.php">Add Students</a>
     <a class ="btn btn-info pull-right" href="index.php">Back</a>
     </h2>
     <div class ="panel panel-body">
+    <form action = "index.php" method="Post">
     <table class ="table table-striped">
     <tr>
-    <th>Dates</th> <th>Roll Number</th><th>Show Attendance</th>
+    <th>Student Name</th> <th>Roll No.</th> <th>Year</th> <th>Phone No.</th> <th>Attendance Status</th>
     </tr>
     <?php 
-        $result = mysqli_query($con,"select distinct date from attendance_records");
+        $result = mysqli_query($con,"select *from attendance_records where date = $_POST[date]");
         $rollnumber=0;
+        $counter=0;
         while($row= mysqli_fetch_array($result))
         {
             $rollnumber++;
             
             ?>
             <tr>
-            <td> <?php echo $rollnumber; ?></td>
-            <td> <?php echo $row[date] ;?></td>
-            <td> <form action ="show_attendance.php" method ="POST">
-            <input type ="hidden" value= "<?php echo $row['date']?>" name="date">
-            <input type ="submit" value="show attendance" class="btn btn-primary">
-            </form></td>
+            <td> <?php echo $rollnumber; ?></td> 
+            <td> <?php echo $row[student_name] ;?></td>
+            <td> <?php echo $row[phone_number] ;?></td>
 
-
+            <td> 
+            <input type="radio" name="attendance_status[<?php echo $counter; ?>]" value="Present"> 
+            <?php if($row['attendance_status']=="Present") 
+            echo"checked = checked "; ?>Present
+            <input type="radio" name="attendance_status[<?php echo $counter; ?>]" value="Absent">
+            <?php if($row['attendance_status ']=="Absent") 
+            echo"checked = checked "; ?>Absent
+            
+            
+            </td>
             </tr>
             <?php
-            }
+            $counter++;
+        }
     ?>
     </table>
 
+    <input  type="submit" name="submit" value="submit" class="btn btn-primary">
     </form>
 
 
